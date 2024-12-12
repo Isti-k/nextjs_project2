642998a0b22a0088ecffe42f1868d398965a40a4
Isti-k <kiscserkes03@gmail.com>
2024-12-12 11:15:09 +0100

    nav, hamburger

--- src/app/componens/Navbar.tsx
+++ src/app/componens/Navbar.tsx
@@ -6,7 +6,7 @@ import Hamburger from './hamburger-spin';
 function Navbar() {
   return (
     <header className='bg-cover bg-center' >
-        <nav className="flex justify-between items-center text-white backdrop-blur-sm bg-opacity-30 p-6 rounded-bl-3xl rounded-br-3xl overflow-hidden">
+        <nav className="flex justify-between items-center text-white backdrop-blur-sm bg-opacity-30 p-6 rounded-bl-3xl rounded-br-3xl overflow-hidden border-b border-white/30">
 
         <div className="flex">
           <Link href="/" className="m-4">
@@ -40,7 +40,7 @@ function Navbar() {
           </Link>
         </div>
         <div>
-         <Hamburger />
+         
         </div>
       </nav>
     </header>
--- src/app/componens/hamburger-spin.tsx
+++ src/app/componens/hamburger-spin.tsx
@@ -1,17 +1,66 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
-const Hamburger = () => {
-  const [opened, setOpened] = useState(false);
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className={`tham tham-e-squeeze tham-w-6`}>
-      <div className="tham-box">
-        <div className="tham-inner" />
+    <div className="relative">
+      <div
+        className={`tham tham-e-squeeze tham-w-6 ${isOpen ? "tham-active" : ""} flex items-center justify-center absolute top-5 right-5 z-50`}
+        onClick={toggleMenu}
+      >
+        <div className="tham-box">
+          <div className="tham-inner bg-white" />
+        </div>
+      </div>
+
+      <div
+        className={`fixed top- right-0 w-64 h-auto backdrop-blur-sm shadow-lg border-l border-white/30 transform transition-transform duration-500 rounded-lg ${
+          isOpen ? "translate-x-0" : "translate-x-full"}`}>
+
+        <ul className="flex flex-col p-4 space-y-4">
+          <li className="relative border-b border-white/30 pb-2 group">
+            <a
+              href="#"
+              className="text-white group-hover:text-transparent group-hover:bg-transparent group-hover:backdrop-blur-none group-hover:opacity-100 transition-all duration-300"
+            >
+              Menetrendek
+            </a>
+          </li>
+          <li className="relative border-b border-white/30 pb-2 group">
+            <a
+              href="#"
+              className="text-white group-hover:text-transparent group-hover:bg-transparent group-hover:backdrop-blur-none group-hover:opacity-100 transition-all duration-300"
+            >
+              Galéria
+            </a>
+          </li>
+          <li className="relative border-b border-white/30 pb-2 group">
+            <a
+              href="#"
+              className="text-white group-hover:text-transparent group-hover:bg-transparent group-hover:backdrop-blur-none group-hover:opacity-100 transition-all duration-300"
+            >
+              GY.I.K.
+            </a>
+          </li>
+          <li className="relative group">
+            <a
+              href="#"
+              className="text-white group-hover:text-transparent group-hover:bg-transparent group-hover:backdrop-blur-none group-hover:opacity-100 transition-all duration-300"
+            >
+              Tájékoztatás
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
-}
+};
 
-export default Hamburger
+export default HamburgerMenu;
